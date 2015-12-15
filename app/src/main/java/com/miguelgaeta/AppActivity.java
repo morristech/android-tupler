33c63b962f669e4ca8ec7ed02ee6e4790d24ace3
mrkcsc <miguel@miguelgaeta.com>
2015-12-15 15:04:00 -0800

    Throw in sample app activity.

---
+++ app/src/main/java/com/miguelgaeta/AppActivity.java
@@ -0,0 +1,19 @@
+package com.miguelgaeta;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Miguel Gaeta on 12/15/15.
+ */
+@SuppressWarnings("DefaultFileTemplate")
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+}
