6b09f8821ce5111feb6827d5a41ab1a444482538
mrkcsc <miguel@miguelgaeta.com>
2015-12-15 14:33:21 -0800

    Added tuple files.

---
+++ tupler/src/main/java/com/miguelgaeta/tupler/Tuple1.java
@@ -0,0 +1,15 @@
+package com.miguelgaeta.tupler;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Created by Miguel Gaeta on 5/20/15.
+ */
+@SuppressWarnings("UnusedDeclaration") @AllArgsConstructor(access = AccessLevel.PACKAGE) @ToString @EqualsAndHashCode
+public class Tuple1<A> {
+
+    public final A d1;
+}
---
+++ tupler/src/main/java/com/miguelgaeta/tupler/Tuple2.java
@@ -0,0 +1,16 @@
+package com.miguelgaeta.tupler;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Created by Miguel Gaeta on 5/20/15.
+ */
+@SuppressWarnings("UnusedDeclaration") @AllArgsConstructor(access = AccessLevel.PACKAGE) @ToString @EqualsAndHashCode
+public class Tuple2<A, B> {
+
+    public final A d1;
+    public final B d2;
+}
